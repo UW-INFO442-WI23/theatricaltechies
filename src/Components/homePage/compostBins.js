ec935204ad3dfff4c6a2772bbe03905eb2359280
ryjtoh <60204869+ryjtoh@users.noreply.github.com>
2023-03-08 11:47:51 -0800

    changed noncompostable bin color

--- src/Components/homePage/compostBins.js
+++ src/Components/homePage/compostBins.js
@@ -11,19 +11,21 @@ const CompostBins = () => {
     { id: 6, content: "meat.png" },
   ]);
   const [dragging, setDragging] = useState(false);
-  const [binState, setBinState] = useState("closedbin.svg")
-  const [nonCompostBinState, setNonCompostBinState] = useState("closedNonCompostbin.svg")
+  const [binState, setBinState] = useState("closedbin.svg");
+  const [nonCompostBinState, setNonCompostBinState] = useState(
+    "closedNonCompostbin.svg"
+  );
 
   const handleDragStart = (event, id) => {
     setDragging(id);
-    setBinState("openbin.svg")
-    setNonCompostBinState("openNonCompostbin.svg")
+    setBinState("openbin.svg");
+    setNonCompostBinState("openNonCompostbin.svg");
   };
 
   const handleDragEnd = (event) => {
     setDragging(false);
-    setBinState("closedbin.svg")
-    setNonCompostBinState("closedNonCompostbin.svg")
+    setBinState("closedbin.svg");
+    setNonCompostBinState("closedNonCompostbin.svg");
   };
 
   const handleDrop1 = (event) => {
@@ -33,11 +35,11 @@ const CompostBins = () => {
         (container) => container.id !== dragging
       );
       setContainers(updatedContainers);
-      alert("Correct")
+      alert("Correct");
     }
     setDragging(false);
-    setBinState("closedbin.svg")
-    setNonCompostBinState("closedNonCompostbin.svg")
+    setBinState("closedbin.svg");
+    setNonCompostBinState("closedNonCompostbin.svg");
   };
 
   const handleDrop2 = (event) => {
@@ -47,11 +49,11 @@ const CompostBins = () => {
         (container) => container.id !== dragging
       );
       setContainers(updatedContainers);
-      alert("Correct")
+      alert("Correct");
     }
     setDragging(false);
-    setBinState("closedbin.svg")
-    setNonCompostBinState("closedNonCompostbin.svg")
+    setBinState("closedbin.svg");
+    setNonCompostBinState("closedNonCompostbin.svg");
   };
 
   const handleDrop3 = (event) => {
@@ -61,11 +63,11 @@ const CompostBins = () => {
         (container) => container.id !== dragging
       );
       setContainers(updatedContainers);
-      alert("Correct")
+      alert("Correct");
     }
     setDragging(false);
-    setBinState("closedbin.svg")
-    setNonCompostBinState("closedNonCompostbin.svg")
+    setBinState("closedbin.svg");
+    setNonCompostBinState("closedNonCompostbin.svg");
   };
 
   const handleDrop4 = (event) => {
@@ -75,11 +77,11 @@ const CompostBins = () => {
         (container) => container.id !== dragging
       );
       setContainers(updatedContainers);
-      alert("Correct")
+      alert("Correct");
     }
     setDragging(false);
-    setBinState("closedbin.svg")
-    setNonCompostBinState("closedNonCompostbin.svg")
+    setBinState("closedbin.svg");
+    setNonCompostBinState("closedNonCompostbin.svg");
   };
 
   const handleDragOver = (event) => {
@@ -91,8 +93,12 @@ const CompostBins = () => {
       <div className="instructions">
         <div className="sectionParagraph">
           <h1>INSTRUCTIONS</h1>
-          <p>It's time to practice composting! Drag each item into the correct compost bin based on how long you think it will take to compost. 
-            If the item is placed correctly, the item will disappear and be composted otherwise the item will remain in the same place. </p>
+          <p>
+            It's time to practice composting! Drag each item into the correct
+            compost bin based on how long you think it will take to compost. If
+            the item is placed correctly, the item will disappear and be
+            composted otherwise the item will remain in the same place.{" "}
+          </p>
         </div>
       </div>
       <div className="containers">
@@ -114,43 +120,67 @@ const CompostBins = () => {
           display: "flex",
           justifyContent: "space-evenly",
           border: "1px black",
-          paddingBottom: "10rem"
+          paddingBottom: "10rem",
         }}
       >
         <div
           className={`droppable-container ${dragging ? "active" : ""}`}
           onDrop={handleDrop1}
           onDragOver={handleDragOver}
-          style={{ display: "flex", justifyContent: "center", flexDirection: "column", paddingBottom: "8rem", width: "200px"}}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            paddingBottom: "8rem",
+            width: "200px",
+          }}
         >
-          <img src={binState} alt="compostbin"/>
+          <img src={binState} alt="compostbin" />
           <p style={{ paddingTop: "1rem" }}>6 MONTHS</p>
         </div>
         <div
           className={`droppable-container ${dragging ? "active" : ""}`}
           onDrop={handleDrop2}
           onDragOver={handleDragOver}
-          style={{ display: "flex", justifyContent: "center", flexDirection: "column", paddingBottom: "8rem", width: "200px" }}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            paddingBottom: "8rem",
+            width: "200px",
+          }}
         >
-          <img src={binState} alt="compostbin"/>
+          <img src={binState} alt="compostbin" />
           <p style={{ paddingTop: "1rem" }}>12 MONTHS</p>
         </div>
         <div
           className={`droppable-container ${dragging ? "active" : ""}`}
           onDrop={handleDrop3}
           onDragOver={handleDragOver}
-          style={{ display: "flex", justifyContent: "center", flexDirection: "column", paddingBottom: "8rem", width: "200px" }}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            paddingBottom: "8rem",
+            width: "200px",
+          }}
         >
-          <img src={binState} alt="compostbin"/>
+          <img src={binState} alt="compostbin" />
           <p style={{ paddingTop: "1rem" }}>2 YEARS +</p>
         </div>
         <div
           className={`droppable-container ${dragging ? "active" : ""}`}
           onDrop={handleDrop4}
           onDragOver={handleDragOver}
-          style={{ display: "flex", justifyContent: "center", flexDirection: "column", paddingBottom: "8rem", width: "200px" }}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            paddingBottom: "8rem",
+            width: "200px",
+          }}
         >
-          <img src={nonCompostBinState} alt="noncompostbin"/>
+          <img src={binState} alt="noncompostbin" />
           <p style={{ paddingTop: "1rem" }}>NON-COMPOSTABLE</p>
         </div>
       </div>
